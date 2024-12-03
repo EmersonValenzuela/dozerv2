dc418de1bc39061f34caea82df2f78c89ce32064
Emerson Valenzuela <144744129+SystemsGV@users.noreply.github.com>
2024-12-03 15:25:45 -0500

    :rocket: Added textbox filter course

--- resources/views/home/list.blade.php
+++ resources/views/home/list.blade.php
@@ -31,56 +31,60 @@
                     @foreach ($programTypes as $programType)
                         <div class="tab-pane fade @if ($loop->first) show active @endif"
                             id="tab-{{ $programType->id_program_type }}" role="tabpanel">
-                            <!-- Filtros -->
-                            <div class="d-flex flex-row mb-3 pt-3 justify-content-between text-white">
-                                <!-- Aquí puedes añadir los filtros si es necesario -->
+                            <!-- Filtro -->
+                            <div class="d-flex flex-row mb-3 pt-3 justify-content-start align-items-center text-white">
+                                <input type="text" class="form-control form-control-lg w-25 me-3 filter-input"
+                                    placeholder="Filtrar ..." data-tab="{{ $programType->id_program_type }}">
                             </div>
 
                             <!-- Contenido de cursos -->
                             @php
-                                // Filtrar los cursos por el tipo de programa actual
                                 $filteredCourses = $courses->filter(function ($course) use ($programType) {
                                     return $course->program_type_id == $programType->id_program_type;
                                 });
                             @endphp
 
-                            @if ($filteredCourses->isEmpty())
-                                <div class="style-course text-white align-items-start">
-                                    <div class="text-white">
-                                        No hay cursos disponibles en este programa.
-                                    </div>
-                                </div>
-                            @else
-                                @foreach ($filteredCourses as $course)
-                                    <div class="style-course text-white align-items-start">
-                                        <div class="d-flex mb-3">
-                                            <div class="d-flex flex-column mb-3 px-2 w-50">
-                                                <div class="d-flex flex-row mb-3 font-programa">
-                                                    <div class=" ">Programa:</div>
-                                                    <div class="">{{ $programType->name }}</div>
-                                                </div>
-                                                <div class="nombr-cap">
-                                                    {{ $course->course_or_event }}
-                                                </div>
-                                                <div class="sub-text">
-                                                    Estudiantes: <span>Total: {{ $course->students_count }}</span>
-                                                </div>
-                                                <div class="sub-text">
-                                                    Correos enviados: <span>{{ $course->emails_sent }}</span>
-                                                </div>
-                                            </div>
-                                            <div class="ms-auto p-2 align-self-center btn-certi">
-                                                <a class="text-white"
-                                                    href="{{ route('home.course', $course->code_course) }}">Ver</a>
-                                            </div>
+                            <div class="courses-container">
+                                @if ($filteredCourses->isEmpty())
+                                    <div class="style-course text-white align-items-start no-courses-message">
+                                        <div class="text-white">
+                                            No hay cursos disponibles en este programa.
                                         </div>
                                     </div>
-                                @endforeach
-                            @endif
+                                @else
+                                    @foreach ($filteredCourses as $course)
+                                        <div class="style-course text-white align-items-start course-item"
+                                            data-name="{{ strtolower($course->course_or_event) }}">
+                                            <div class="d-flex mb-3">
+                                                <div class="d-flex flex-column mb-3 px-2 w-50">
+                                                    <div class="d-flex flex-row mb-3 font-programa">
+                                                        <div class="">Programa: &nbsp;</div>
+                                                        <div class=""> {{ $programType->name }}</div>
+                                                    </div>
+                                                    <div class="nombr-cap">
+                                                        {{ $course->course_or_event }}
+                                                    </div>
+                                                    <div class="sub-text">
+                                                        Estudiantes: <span>Total: {{ $course->students_count }}</span>
+                                                    </div>
+                                                    <div class="sub-text">
+                                                        Fecha: <span>{{ $course->created_at->format('d/m/Y') }}</span>
+                                                    </div>
+                                                </div>
+                                                <div class="ms-auto p-2 align-self-center btn-certi">
+                                                    <a class="text-white"
+                                                        href="{{ route('home.course', $course->code_course) }}">Ver</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                @endif
+                            </div>
                         </div>
                     @endforeach
                 </div>
             </div>
+
         </div>
     </div>
 @endsection()
@@ -89,4 +93,33 @@
 @endsection()
 
 @section('scripts')
+    <script>
+        document.querySelectorAll('.filter-input').forEach(input => {
+            input.addEventListener('input', function() {
+                const filterValue = this.value.toLowerCase();
+                const tabId = this.getAttribute('data-tab');
+                const coursesContainer = document.querySelector(`#tab-${tabId} .courses-container`);
+                const courseItems = coursesContainer.querySelectorAll('.course-item');
+                let visibleCount = 0;
+
+                courseItems.forEach(course => {
+                    const courseName = course.getAttribute('data-name');
+                    if (courseName.includes(filterValue)) {
+                        course.style.display = '';
+                        visibleCount++;
+                    } else {
+                        course.style.display = 'none';
+                    }
+                });
+
+                // Mostrar o ocultar mensaje de "No hay cursos disponibles"
+                const noCoursesMessage = coursesContainer.querySelector('.no-courses-message');
+                if (visibleCount === 0) {
+                    noCoursesMessage.style.display = '';
+                } else {
+                    noCoursesMessage.style.display = 'none';
+                }
+            });
+        });
+    </script>
 @endsection
